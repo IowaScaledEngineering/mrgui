356bf94a022f4da0aa2df23bace22ef29b6e497a
Michael Petersen <railfan@drgw.net>
2014-12-11 04:25:00 +0000

    Needed file

---
+++ src/avrinfo.h
@@ -0,0 +1,41 @@
+/*************************************************************************
+Title:    MRGui
+Authors:  Michael Petersen <railfan@drgw.net>
+          Nathan D. Holmes <maverick@drgw.net>
+File:     avrinfo.h
+License:  GNU General Public License v3
+
+LICENSE:
+    Copyright (C) 2014 Michael Petersen & Nathan Holmes
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+*************************************************************************/
+
+#ifndef AVRINFO_H
+#define AVRINFO_H
+
+typedef struct
+{
+	const char* part_name;
+	const uint32_t eeprom_size;
+	const char* avrdude_cmd;
+
+} AVRInfo;
+
+const AVRInfo avrinfo[] = 
+{
+	{"atmega328", 1024, "some_command_here"},
+	{"atmega48", 256, "some_command_here2"},
+};
+
+#endif
+
