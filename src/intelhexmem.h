873ab50360bb1d8ce42053d04b5945122cc6dc5a
Michael Petersen <railfan@drgw.net>
2014-12-19 18:36:09 +0000

    Changed malloc.h to stdlib.h

--- src/intelhexmem.h
+++ src/intelhexmem.h
@@ -2,11 +2,6 @@
 #define _INTELHEXMEM_H_
 
 #include <stdio.h>
-#if defined(__APPLE__) || defined(__FreeBSD__)
-#include <malloc/malloc.h> 
-#else
-#include <malloc.h>
-#endif
 #include <stdint.h>
 
 class IntelHexMemory
